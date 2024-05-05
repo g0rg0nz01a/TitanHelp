e7f710d2a92c1d2374609d409f6a9bc0dea029df
Srhwlsn <swilso79@live.spcollege.edu>
2024-05-05 19:44:37 -0400

    Change Label for Title to User Role
    Change form Label Title to display User Role.
    Additional Comments for additional form items.
    Select List for Role and creating a Department option.

--- TitanHelp/Models/TicketModel.cs
+++ TitanHelp/Models/TicketModel.cs
@@ -7,8 +7,14 @@ public class Ticket
 {
     public int ID { get; set; }
     public string? Name { get; set; }
+    // Change Title to int RoleID for Student = 0, Faculty = 1, Staff = 2
+    [Display(Name="User Role")]
     public string? Title { get; set; }
     [DataType(DataType.Date)]
     public DateTime Date { get; set; }
+    // For Department Selection 
+    //DepartmentID: Account Management, Technical Issues, Billing and Payments, Other
+   // [Display(Name = "Department")]
+   // public int DepartmentID { get; set}
     public string? Description { get; set; }
 }
