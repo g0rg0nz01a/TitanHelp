77bc5aacf59c03d992d9b7cd8af46b621dd36ddf
Srhwlsn <swilso79@live.spcollege.edu>
2024-05-06 17:34:15 -0400

    Support and Buttons
    Fixed Support Page View and added buttons. Additional .css to follow.

--- TitanHelp/Controllers/HomeController.cs
+++ TitanHelp/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Support()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
